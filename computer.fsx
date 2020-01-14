d571168a49e2cabd1a7f23df3b818d8f9b2f006f
JohanEklund <JohanEklund@hotmail.com>
2020-01-14 22:59:08 +0100

    day5b

--- computer.fsx
+++ computer.fsx
@@ -41,6 +41,44 @@ let getOutput1 runtime =
     let output = runtime.program.[runtime.pointer+1]
     Output1{ output={Value=output; Mode=paramMode}}
 
+let getJumpTrue runtime =
+    let ins = runtime.program.[runtime.pointer]
+    let firstMode = firstParamMode ins
+    let secondMode = secondParamMode ins
+    JumpTrue{
+        p1 = { Value=runtime.program.[runtime.pointer+1]; Mode=secondMode };
+        p2 = { Value=runtime.program.[runtime.pointer+2]; Mode=firstMode };
+    }
+
+let getJumpFalse runtime =
+    let ins = runtime.program.[runtime.pointer]
+    let firstMode = firstParamMode ins
+    let secondMode = secondParamMode ins
+    JumpFalse{
+        p1 = { Value=runtime.program.[runtime.pointer+1]; Mode=secondMode };
+        p2 = { Value=runtime.program.[runtime.pointer+2]; Mode=firstMode };
+    }
+
+let getLessThan runtime =
+    let ins = runtime.program.[runtime.pointer]
+    let firstMode = firstParamMode ins
+    let secondMode = secondParamMode ins
+    LessThan{
+        p1 = { Value=runtime.program.[runtime.pointer+1]; Mode=secondMode};
+        p2 = { Value=runtime.program.[runtime.pointer+2]; Mode=firstMode};
+        output = runtime.program.[runtime.pointer+3]
+    }
+
+let getEquals runtime =
+    let ins = runtime.program.[runtime.pointer]
+    let firstMode = firstParamMode ins
+    let secondMode = secondParamMode ins
+    Equals{
+        p1 = { Value=runtime.program.[runtime.pointer+1]; Mode=secondMode};
+        p2 = { Value=runtime.program.[runtime.pointer+2]; Mode=firstMode};
+        output = runtime.program.[runtime.pointer+3]
+    }
+
 let getCurrentInstruction runtime =
     runtime.program.[runtime.pointer]
 
@@ -51,12 +89,20 @@ let getOperation runtime =
         | 1 | 2 -> getTwoParamOperation runtime
         | 3 -> getInput1 runtime
         | 4 -> getOutput1 runtime
+        | 5 -> getJumpTrue runtime
+        | 6 -> getJumpFalse runtime
+        | 7 -> getLessThan runtime
+        | 8 -> getEquals runtime
 
 let incrementPointer ins curr =
     match ins with    
     | AddOrMultiply (_) -> curr + 4
     | Input1 (_) -> curr + 2
     | Output1 (_) -> curr + 2
+    | JumpTrue (_) -> curr + 3
+    | JumpFalse (_) -> curr + 3
+    | LessThan (_) -> curr + 4
+    | Equals (_) -> curr + 4
 
 let paramValue param (program: int list) =
     param.Mode
@@ -64,29 +110,74 @@ let paramValue param (program: int list) =
         | ParamMode.Positional -> program.[param.Value]
         | ParamMode.Immediate -> param.Value
     
-let doAddOrMultiply ins (p: AddOrMultiply) program =
-    let p1 = paramValue p.p1 program
-    let p2 = paramValue p.p2 program
+let replaceProgram runtime newProgram =
+    { program = newProgram; pointer=runtime.pointer; input=runtime.input }
+
+let doAddOrMultiply ins (p: AddOrMultiply) runtime =
+    let p1 = paramValue p.p1 runtime.program
+    let p2 = paramValue p.p2 runtime.program
     let res = performAddOrMultiply ins p1 p2
-    replaceValueAt program p.output res
+    replaceValueAt runtime.program p.output res
+    |> replaceProgram runtime
 
-let Op3 program (op : Input1) =
-    replaceValueAt program op.output op.input
+let Op3 runtime (op : Input1) =
+    replaceValueAt runtime.program op.output op.input
+    |> replaceProgram runtime
 
-let Op4 program (op : Output1) =
-    let v = paramValue op.output program
+let Op4 runtime (op : Output1) =
+    let v = paramValue op.output runtime.program
     printfn "Output:\t%i" v
-    program
+    runtime
+
+let doJumpTrue runtime (op: JumpTrue) =
+    paramValue op.p1 runtime.program
+    |> function
+        | 0 -> runtime
+        | _ -> { program = runtime.program; pointer = (paramValue op.p2 runtime.program); input=runtime.input }
+    
+let doJumpFalse runtime (op: JumpFalse) =
+    paramValue op.p1 runtime.program
+    |> function
+        | 0 -> { program = runtime.program; pointer = (paramValue op.p2 runtime.program); input=runtime.input }
+        | _ -> runtime
+
+let doLessThan runtime (op: LessThan) =
+    let p1 = paramValue op.p1 runtime.program
+    let p2 = paramValue op.p2 runtime.program
+    let value = match p1 < p2 with
+                    | true -> 1
+                    | false -> 0
+    replaceValueAt runtime.program op.output value
+    |> replaceProgram runtime                
+
+let doEquals runtime (op: Equals) =
+    let p1 = paramValue op.p1 runtime.program
+    let p2 = paramValue op.p2 runtime.program
+    let value = match p1 = p2 with
+                    | true -> 1
+                    | false -> 0
+    replaceValueAt runtime.program op.output value
+    |> replaceProgram runtime                
+
+let handlePointer newPointer oldPointer op =
+    match newPointer = oldPointer with
+    | true -> incrementPointer op oldPointer
+    | false -> newPointer
+    
 
 let handleInstruction runtime =
     let ins = getCurrentInstruction runtime
     let op = getOperation runtime
-    let newProgram = match op with
-                        | AddOrMultiply (x) -> doAddOrMultiply ins x runtime.program
-                        | Input1 (x) -> Op3 runtime.program x
-                        | Output1 (x) -> Op4 runtime.program x
-    let newPointer = incrementPointer op runtime.pointer
-    {program=newProgram; pointer=newPointer; input=runtime.input}
+    let newRuntime = match op with
+                        | AddOrMultiply (x) -> doAddOrMultiply ins x runtime
+                        | Input1 (x) -> Op3 runtime x
+                        | Output1 (x) -> Op4 runtime x
+                        | JumpTrue (x) -> doJumpTrue runtime x
+                        | JumpFalse (x) -> doJumpFalse runtime x
+                        | LessThan (x) -> doLessThan runtime x
+                        | Equals (x) -> doEquals runtime x
+    let newPointer = handlePointer newRuntime.pointer runtime.pointer op
+    {program=newRuntime.program; pointer=newPointer; input=runtime.input}
 
 let rec handleProgram runtime =
     let a = getCurrentInstruction runtime
--- computerTypes.fsx
+++ computerTypes.fsx
@@ -10,9 +10,21 @@ type Input1 = { input: int; output: int }
 
 type Output1 = { output: Parameter }
 
+type JumpTrue = { p1: Parameter; p2: Parameter }
+
+type JumpFalse = { p1: Parameter; p2: Parameter }
+
+type LessThan = { p1: Parameter; p2: Parameter; output: int }
+
+type Equals = { p1: Parameter; p2: Parameter; output: int }
+
 type Operation =
     | AddOrMultiply of AddOrMultiply
     | Input1 of Input1
     | Output1 of Output1
+    | JumpTrue of JumpTrue
+    | JumpFalse of JumpFalse
+    | LessThan of LessThan
+    | Equals of Equals
 
 type Runtime = { program: int list; pointer: int; input: int }
